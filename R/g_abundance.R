619cc3d8aec0569a24eec5c370cb6d78595cbae8
Nertekkad <61397302+Nertekkad@users.noreply.github.com>
2023-10-23 16:12:53 -0600

    g_abundance

--- R/g_abundance.R
+++ R/g_abundance.R
@@ -1,18 +1,22 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+# g_abundance
 
-hello <- function() {
-  print("Hello, world!")
+g_abundance<-function(layer_mat, g){
+  # Relative abundances quantification
+  net_ab<-colSums(layer_mat)
+  net_ab<-as.numeric((net_ab)/net_ab[which.max(net_ab)])
+  # Colors for each abundance level
+  abcol<-topo.colors(5)
+  # Color assignment to the nodes depending on their abundance
+  ab1<-which(net_ab > 0 & net_ab <= 0.2)
+  V(g)[ab1]$rel_ab<-abcol[5]
+  ab2<-which(net_ab > 0.2 & net_ab <= 0.4)
+  V(g)[ab2]$rel_ab<-abcol[4]
+  ab3<-which(net_ab > 0.4 & net_ab <= 0.6)
+  V(g)[ab3]$rel_ab<-abcol[3]
+  ab4<-which(net_ab > 0.6 & net_ab <= 0.8)
+  V(g)[ab4]$rel_ab<-abcol[2]
+  ab5<-which(net_ab > 0.8 & net_ab <= 1)
+  V(g)[ab5]$rel_ab<-abcol[1]
+  vertex.attributes(g)$rel_ab
+  return(g)
 }
