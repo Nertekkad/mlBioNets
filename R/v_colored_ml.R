93f1ad4c47085216618edbf3966ae8d95f061c4d
Nertekkad <61397302+Nertekkad@users.noreply.github.com>
2023-11-13 04:05:15 -0600

    Corrections

--- R/v_colored_ml.R
+++ R/v_colored_ml.R
@@ -5,4 +5,5 @@ v_colored_ml<-function(g.list, T_table, g_tax, p_tax, g_colors){
     g.list[[i]]<-v_colored(g.list[[i]], T_table, g_tax,
                            p_tax, g_colors)
   }
+  return(g.list)
 }
