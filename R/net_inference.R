50e39458eb3ae29b32970d07a0c71abf9b178c94
Nertekkad <61397302+Nertekkad@users.noreply.github.com>
2024-01-29 19:01:18 -0600

    net_inference

---
+++ R/net_inference.R
@@ -0,0 +1,19 @@
+net_inference<-function(taxa_abs, method, p){
+  require(SpiecEasi)
+  require(igraph)
+  require(minet)
+  if(method == "sparcc"){
+    sparccNet <- sparcc(taxa_abs)
+    sparccNet <- abs(sparccNet$Cor) >= p
+    taxa_net <- adj2igraph(sparccNet)
+    vertex.attributes(taxa_net) <- list(name = colnames(taxa_abs))
+    return(taxa_net)
+  }
+  else if(method == "aracne"){
+    mim <- build.mim(taxa_abs,estimator="spearman")
+    aracnet <- aracne(mim)
+    taxa_net <- graph.adjacency(aracnet)
+    taxa_net <- as.undirected(taxa_net)
+    return(taxa_net)
+  }
+}
