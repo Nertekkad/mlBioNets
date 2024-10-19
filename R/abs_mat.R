e904047f45cf4979b21668414cc82860e2cf2b15
valeria <vhernandez41@alumnos.uaq.mx>
2024-10-18 21:21:03 -0600

    trial

--- R/abs_mat.R
+++ R/abs_mat.R
@@ -3,7 +3,7 @@
 #' @usage abs_mat (abs.list, g.list, n)
 #' @param abs.list Abundance list, the same lists you used to generate the network.
 #' @param g.list List of igraph objects (multilayer network).
-#' @param n Vertex number. Notice that if n>20 the nodes won't show so big.
+#' @param n Vertex number. Notice that if n>20 the nodes won't show too big.
 #' @return Returns a matrix, each column is a layer, and the values of the rows corresponds to the size of each node.
 #' @examples
 #'
